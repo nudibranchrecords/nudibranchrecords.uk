c8f33e68df50d98906f24f2162e33fa3eddb623f
Alex Kempton <alex@funwithtriangles.net>
2018-02-07 14:36:28 +0000

    Clip mask and transitions

--- app/index.js
+++ app/index.js
@@ -1,17 +1,16 @@
-/**
- * Application entry point
- */
-
-// Load application styles
 import 'styles/index.scss'
 import GlslCanvas from 'glslCanvas'
 import frag from 'frag.glsl'
 import 'images/logo.svg'
 
-// ================================
-// START YOUR APP HERE
-// ================================
-
 const canvas = document.querySelector('canvas')
 const sandbox = new GlslCanvas(canvas)
 sandbox.load(frag)
+
+window.setTimeout(() => {
+  document.body.classList.add('is-loaded')
+}, 100)
+
+window.setTimeout(() => {
+  document.body.classList.add('no-transition')
+}, 10000)
