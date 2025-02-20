0ebd59baf3a404e6ee46d5988016cf8507856955
Alex Kempton <alex@funwithtriangles.net>
2025-02-20 15:31:25 +0100

    config tweak

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { ViteEjsPlugin } from "vite-plugin-ejs";
 import vitePluginString from "vite-plugin-string";
 
 export default defineConfig({
-  base: "", // Force relative paths for GitHub Pages
+  base: "./", // Force relative paths for GitHub Pages
   plugins: [ViteEjsPlugin(), vitePluginString()],
   build: {
     outDir: "dist",
